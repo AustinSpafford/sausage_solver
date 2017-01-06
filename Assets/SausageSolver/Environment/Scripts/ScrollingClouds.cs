10288bcb0b86da152ff50004e389b72ef977718b
Austin Spafford <austin.spafford@gmail.com>
2017-01-06 14:11:25 -0800

    Adding crude scrolling projected cloud shadows

---
+++ Assets/SausageSolver/Environment/Scripts/ScrollingClouds.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ScrollingClouds : MonoBehaviour
+{
+	public Vector3 LocalVelocity = Vector3.forward;
+	public float WrappingDistance = 1.0f;
+
+	public void Update()
+	{
+		float oldDistance = currentDistance;
+
+		currentDistance += (LocalVelocity.magnitude * Time.deltaTime);
+		currentDistance = (currentDistance % WrappingDistance);
+
+		float updateDistanceDelta = (currentDistance - oldDistance);
+
+		transform.localPosition += (LocalVelocity * updateDistanceDelta);
+	}
+
+	private float currentDistance = 0.0f;
+}
